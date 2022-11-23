a5dbcefe50126cabdef6cfad334a1af40421e310
Azat02 <118878932+Azat02@users.noreply.github.com>
2022-11-23 12:50:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function () {
+        const menu = document.querySelector('.menu'), menuItem = document.querySelectorAll('.menu_item'), hamburger = document.querySelector('.hamburger');
+
+        hamburger.addEventListener('click', function () {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+
+        menuItem.forEach(function (item) {
+            item.addEventListener('click', function () {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            });
+        });
+    });
